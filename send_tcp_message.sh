50345703915753273845f35497a092c35353c54e
conni <cbuschka@gmail.com>
2021-05-25 09:51:23 +0200

    Added sending multiple messages via send_tcp_messsage.sh.

--- send_tcp_message.sh
+++ send_tcp_message.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo -n -e '{ "version": "1.1", "host": "example.org", "short_message": "A short message", "level": 5, "_some_info": "foo" }'"\0" | nc -w1 127.0.0.1 12201
+count=${1:-1}
+
+(for i in $(seq 1 ${count}); do echo -n -e "{ \"version\": \"1.1\", \"host\": \"example.org\", \"short_message\": \"A short message\", \"level\": 5, \"_some_info\": \"foo\", \"_index\": ${i} }\0"; done) | nc -w1 127.0.0.1 12201
